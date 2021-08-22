4943db4e84c794f5e0aacb5a54ca87ab43076ba9
anandsr-dev <89254071+anandsr-dev@users.noreply.github.com>
2021-08-23 01:35:53 +0530

    update 4

--- form.js
+++ form.js
@@ -1,288 +1,124 @@
-<!doctype html>
-<html lang="en">
+let namecheck = false;
+let mobcheck = false;
+let mailcheck = false;
+let feedbackcheck = false;
 
-<head>
+$('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+});
+$(document).ready(function () {
+    $("#fname").keydown(function (ev) {
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="images/icon.png">
+        let namekey = ev.key;
 
+        let input = this.value
+        let prev = input.length - 1
+        let nameregex = /^[A-Za-z ]$/
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        if (ev.which != 8 && ev.which != 46) {
+            if (!nameregex.test(namekey) || (input[prev] == " " && namekey == " ")) {
+                ev.preventDefault();
+            }
+        }
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        if (input.length >= 2) {
+            $("#error1").text("")
+            namecheck = true
+        }
+    })
 
+    $("#fname").blur(function () {
+        var val = this.value
+        var length = val.length
+        if (length < 3) {
+            namecheck = false
+            $("#error1").text("Enter at least 3 characters")
+        }
+    })
 
-    <title>Anand S</title>
-</head>
+    $("#fmob").keydown(function (ev) {
 
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        let mobkey = ev.key
+        let mob = this.value;
+        let mobregex = /^[0-9]$/
+        if (ev.which != 8 && ev.which != 46) {
+            if (!mobregex.test(mobkey)) {
+                ev.preventDefault()
+            }
+        }
+        if (mob.length == 9) {
+            $("#error2").text("")
+            mobcheck = true
+        }
+        if (mob.length == 10) {
+            if (ev.which != 8 && ev.which != 46) {
+                ev.preventDefault()
+            }
+        }
+    })
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link " href="#Home">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="#About">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="#Skills">Skills</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="#Contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    $("#fmob").blur(function () {
+        var val = this.value
+        var length = val.length
+        if (length < 10) {
+            $("#error2").text("Mobile number should be at least 10 digits")
+        }
+    })
 
-        </nav>
+    $("#femail").blur(function () {
 
-    </header>
+        let mail = this.value;
+        let mailregex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/
+        if (mail.match(mailregex)) {
+            mailcheck = true;
+            $("#error3").text("");
+        }
+        else if (mail == "") {
+            $("#error3").text("Don't leave this field empty");
+        }
+        else {
+            $("#error3").text("Invalid email address");
+        }
 
-    <section id="Home">
-        <div class="container-fluid p-5">
-            <div class="row p-5">
-                <div class="col-md-6 col-xs-12 mt-5">
-                    <img src="images/img.png" alt="" class="w-75 float-right pr-3">
-                </div>
-                <div class="col-md-6 col-xs-12 align-self-center mt-5 p-3">
-                    <h1 class="display-4" style="font-weight:550">Hi! I am <span style="color:skyblue">Anand</span>
-                    </h1>
-                    <p class="lead">An aspiring software developer and a tech enthusiast based in Kerala, India</p>
-                </div>
-            </div>
-        </div>
+    })
 
-    </section>
+    $("#feedback").blur(function () {
 
-    <section id="About">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-2">
+        let feed = this.value;
+        if (feed.length < 30) {
+            feedcheck = false;
+            $("#error4").text("Enter atleast 30 characters")
+        }
+        else {
+            feedcheck = true;
+            $("#error4").text("");
+        }
+    })
 
-                </div>
-                <div class="col-md-8">
-                    <div class="jumbotron">
-                        <h1 class="display-4 text-center" style="font-weight:550">About Me</h1>
-                        <p class="lead text-center mt-4">I am a hard-working and ambitious person who is currently
-                            working on some web development projects and improving my skills. I like learning new
-                            things.
-                            My aim is to become a MERN stack developer and a software engineer or data scientist in the
-                            future.<br>
-                            Other than coding I like to play and watch football, tv series, movies, party and play
-                            games.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-2">
+})
 
-                </div>
+$("#gform").submit((e) => {
 
-            </div>
+    e.preventDefault()
+    if (namecheck == true && mobcheck == true && mailcheck == true && feedcheck == true) {
+        $("#submitmsg").text("")
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbzyVZABM5efWcFiVYffrh7BzVWqWADlyPGy26R5eka8NzOKzgCO_wcmtd1dWp8PbabS/exec",
+            data: $("#gform").serialize(),
+            method: "post",
+            success: function (response) {
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error: function (err) {
+                alert("Submit error. Try again")
 
+            }
+        })
+    }
+    else {
+        $("#submitmsg").text("Please fill all the details and try again")
+    }
 
-
-        </div>
-    </section>
-
-    <section id="Skills">
-        <h1 class="display-4 text-center m-5" style="font-weight:550">Skills</h1>
-        <div class="container-fluid pb-5">
-            <div class="row pt-3">
-                <div class="col-md-1">
-
-                </div>
-                <div class="col-md-10">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/c-programming.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center"> C/C++</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/java-coffee-cup-logo--v1.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Java</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/css3.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">HTML/CSS/Javascript</h5>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/mongodb.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">MongoDB</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/nodejs.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">NodeJS</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/plasticine/400/000000/react.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">ReactJS</h5>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="row mt-4">
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/python--v1.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Python</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem">
-                                <img src="https://img.icons8.com/color/480/000000/django.png"
-                                    class="w-75 align-self-center" />
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">Django</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-
-                        </div>
-
-                    </div>
-
-
-                </div>
-                <div class="col-md-1">
-
-                </div>
-            </div>
-
-
-
-
-        </div>
-
-    </section>
-
-    <section id="Contact">
-        <h1 class="display-4 text-center mt-5 pt-5 pb-2" style="font-weight:550">Contact Me</h1>
-        <div class="container-fluid my-5">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="container">
-                        <div class="jumbotron jumbotron-fluid">
-                            <div class="container">
-                                <h4 class="display-4" style="font-size: 180%; font-weight: 400;">Address</h4>
-                                <p class="lead pt-3" style="font-size: 110%;">Amrita<br>
-                                    Puranattukara (PO)<br>
-                                    Thrissur<br>
-                                    Kerala, India - 680551
-                                </p>
-                                <h4 class="display-4" style="font-size: 180%; font-weight: 400;">Phone</h4>
-                                <p class="lead pt-2" style="font-size: 110%;">7025231366</p>
-                                <h4 class="display-4" style="font-size: 180%; font-weight: 400;">E-Mail</h4>
-                                <p class="lead pt-2" style="font-size: 110%;">anands7894@gmail.com</p>
-                                <div class="pt-5">
-                                    <a href="https://www.facebook.com/anand.sa7x/"
-                                        class="fa fa-facebook pr-2 fa-lg"></a>
-                                    <a href="https://www.linkedin.com/in/anand-s-20a137218/"
-                                        class="fa fa-linkedin fa-lg"></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <form id="gform">
-                        <div class=" form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" name="name" id="fname">
-                            <span id="error1"></span>
-                        </div>
-                        <div class="form-group">
-                            <label for="mob">Mobile Number</label>
-                            <input type="text" class="form-control" id="fmob" name="mob" oninput="">
-                            <span id="error2"></span>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email address</label>
-                            <input type="text" class="form-control" id="femail" name="email">
-                            <span id="error3"></span>
-                        </div>
-                        <div class="form-group">
-                            <label for="feedback">Feedback</label>
-                            <textarea class="form-control" name="feedback" id="feedback" rows="3"></textarea>
-                            <span id="error4"></span>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        <div>
-                            <p id="submitmsg"></p>
-                        </div>
-
-                    </form>
-                </div>
-
-            </div>
-        </div>
-
-
-
-    </section>
-
-
-
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
-    <script src="form.js"></script>
-</body>
-
-</html>
+})
